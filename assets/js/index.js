829da863cf31348bac176d4fca03da92d48d5516
Mauricio Ramirez <mau_rr01@hotmail.com>
2024-08-08 04:00:02 -0600

    Se crea menu para moviles y se modifica diseno

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,15 @@
-function toggleMenu() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
+const navbarToggle = mini_navbar.querySelector('#mini-navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#mini-navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.mini-navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
